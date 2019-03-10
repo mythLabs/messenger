3c12453826ab1a5865fc551bbbc38c80728b9d76
mythlabs <amitbisht744@gmail.com>
2019-03-10 12:08:06 +0530

    Added authentication system

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,4 +1,5 @@
 class ChatroomController < ApplicationController
     def index
+        @messages = Message.order(created_at: :desc)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'chatroom#index'
-  get 'login' , to: 'session#new', as: 'login'
+  get 'login' , to: 'session#new'
+  post 'login' , to: 'session#create'
+  delete 'logout', to: 'session#destroy'
+  
   resource :users
   resource :messages
 end
