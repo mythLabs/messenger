1009a89ff31c9dc2fa2b6ab1a1dea33c60ae1872
mythlabs <amitbisht744@gmail.com>
2019-03-10 09:16:30 +0530

    Added user,message resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'amit', password:'password')
+User.create(username: 'json', password:'password')
+User.create(username: 'jenny', password:'password')
